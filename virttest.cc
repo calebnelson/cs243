842ca47db77468a6006e74599451494db3ab0fbf
Caleb Nelson <calebnelson@gmail.com>
2017-11-23 23:49:40 -0800

    Added (commented out) virtest

---
+++ virttest.cc
@@ -0,0 +1,13 @@
+
+#include "virtos.h"
+#include <stdio.h>
+/*
+int main(int argc, char**argv)
+{
+  Timer t;
+  t.begin();
+  for (int i = 0; i < 10000; i++)
+    argc += i;
+  printf("Time: %ld, %d, %s\n", t.delta(), argc, VirtOs::now().c_str());
+}
+*/
