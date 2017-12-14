8181a3ebbf26fdc25eb5de4ffc5fc4cd5dfbe8b9
Caleb Nelson <calebnelson@gmail.com>
2017-12-13 20:50:37 -0800

    Updated C++ code with blocking multiply

--- main.cc
+++ main.cc
@@ -2,59 +2,56 @@
  * main.cc
  *
  *  Created on: Nov 18, 2017
- *      Author: User
+ *      Author: Caleb Nelson
  */
 
 #include "matrixCalc.cc"
 #include "CSVManager.h"
 
 static const int CacheSizes[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 0 };
-				  
-int main(int argc, char *argv[])
-{
-  string name = "Mac";
-  CSVManager csvm1 = CSVManager("int", name, "");
-  int signal1 = 0;
+		
+#define PLAT "Mac"
+
+// Do a run of tests on a data type (e.g. "int"), into a csv
+//		  
+template <typename T>
+void doRun(CSVManager csvm, string dtype, T signal) {
+
   long sum, avg;
   int s, i; 
+  // Loop over buf sizes in .5m intervals
   for (i = 0; (s = CacheSizes[i]) != 0; i++) {
-    int bufSize = s * 500 * 1024; //Multiply by .5 MB
-    matrixSumTimer(bufSize, s*100, signal1, 16, &sum, &avg);
-    csvm1.writeLine(s/2.0, "Sum", s*100, "int", avg);
-    matrixMultiplyTimer(bufSize, signal1, 1, &sum, &avg);
-    csvm1.writeLine(s/2.0, "Multiply", 0, "int", sum);
+    int bufSize = s * 512 * 1024; //Multiply by .5 MB
+    // Do 16 runs of Sum and average the results
+    matrixSumTimer(bufSize, s*100, signal, 16, &sum, &avg);
+    csvm.writeLine(s/2.0, "Sum", s*100, dtype, avg);
+    // Multiply w/o blocking for 2 runs
+    matrixMultiplyTimer(bufSize, 1, signal, 2, &sum, &avg);
+    csvm.writeLine(s/2.0, "Mult01", 0, dtype, sum);
+    // Multiply blocking by 32x32 squares for 2 runs
+    matrixMultiplyTimer(bufSize, 32, signal, 2, &sum, &avg);
+    csvm.writeLine(s/2.0, "Mult32", 0, dtype, sum);
+    // Multiply blocking by 64x64 squares for 2 runs
+    matrixMultiplyTimer(bufSize, 64, signal, 2, &sum, &avg);
+    csvm.writeLine(s/2.0, "Mult64", 0, dtype, sum);
   }
+}
+
+// Main loop runs an identical set of tests for each of four data types
+//
+int main(int argc, char *argv[])
+{
+  string name = PLAT;
+
+  CSVManager csvm1 = CSVManager("int", name, "");
+  doRun(csvm1, "int", (int)0);
+
   CSVManager csvm2 = CSVManager("float", name, "");
-  float signal2 = 0;
-  //long sum, avg;
-  //int s, i; 
-  for (i = 0; (s = CacheSizes[i]) != 0; i++) {
-    int bufSize = s * 500 * 1024; //Multiply by .5 MB
-    matrixSumTimer(bufSize, s*100, signal2, 16, &sum, &avg);
-    csvm2.writeLine(s/2.0, "Sum", s*100, "float", avg);
-    matrixMultiplyTimer(bufSize, signal2, 1, &sum, &avg);
-    csvm2.writeLine(s/2.0, "Multiply", 0, "float", sum);
-  }
+  doRun(csvm2, "float", (float)0);
+
   CSVManager csvm3 = CSVManager("long", name, "");
-  long signal3 = 0;
-  //long sum, avg;
-  //int s, i; 
-  for (i = 0; (s = CacheSizes[i]) != 0; i++) {
-    int bufSize = s * 500 * 1024; //Multiply by .5 MB
-    matrixSumTimer(bufSize, s*100, signal3, 16, &sum, &avg);
-    csvm3.writeLine(s/2.0, "Sum", s*100, "long", avg);
-    matrixMultiplyTimer(bufSize, signal3, 1, &sum, &avg);
-    csvm3.writeLine(s/2.0, "Multiply", 0, "long", sum);
-  }
+  doRun(csvm3, "long", (long)0);
+
   CSVManager csvm4 = CSVManager("char", name, "");
-  char signal4 = 0;
-  //long sum, avg;
-  //int s, i; 
-  for (i = 0; (s = CacheSizes[i]) != 0; i++) {
-    int bufSize = s * 500 * 1024; //Multiply by .5 MB
-    matrixSumTimer(bufSize, s*100, signal4, 16, &sum, &avg);
-    csvm4.writeLine(s/2.0, "Sum", s*100, "char", avg);
-    matrixMultiplyTimer(bufSize, signal4, 1, &sum, &avg);
-    csvm4.writeLine(s/2.0, "Multiply", 0, "char", sum);
-  }
+  doRun(csvm4, "char", (char)0);
 }
--- matrixCalc.cc
+++ matrixCalc.cc
@@ -6,8 +6,17 @@
 #include  "virtos.h" //will eventually replace this with OS code
 using namespace std;
 
+#define min(a,b) ((a)<(b)?(a):(b))
+
+// matrixSum() sums up the columsn of a matrix into a results row
+//   matrix is of type <signal>, total #bytes <size>, columns <cols>
+//
+// Allocates a block of contiguous space, calculates #rows including an
+// additional results row at the end, fills the matrix wih values, then 
+// sums each column into the results row.
+//
 template <typename T>
-void matrixSum(int size, int cols, T signal){ //T signal only serves to designate the variable type for the function - it doesn't do anything inside the function itself
+int matrixSum(int size, int cols, T signal){ 
 
   int numElements = size/sizeof(T);
   int rows = (numElements / cols) - 1;
@@ -18,11 +27,10 @@ void matrixSum(int size, int cols, T signal){ //T signal only serves to designat
   T *res = buf + rows*cols;
 
   // Set everything to an initial value
-  int r, c;                                                                                                     
-  for (r = 0; r < rows; r++) {
-    T *rp = buf + r*cols;
-    for (c = 0; c < cols; c++) {
-      rp[c] = (T)(r+c);
+  int r, c, x;                                                                                                     
+  for (x = 0, r = 0; r < rows; r++) {
+    for (c = 0; c < cols; c++, x++) {
+      buf[x] = (T)(r+c) + signal;
     }
   }
 
@@ -35,75 +43,111 @@ void matrixSum(int size, int cols, T signal){ //T signal only serves to designat
     res[c] = sum;
   }
 
-  // Sum the total result line to make sure compiler doesn't optimize code away                                                             
+  // Do something with the data so compiler doesn't optimize code away                                                             
   int total = 0;
   for (c = 0; c < cols; c++) total += res[c];
 
   free(buf);
+  return total;
 }
 
+// Time <repitition> runs of Sum, returning total and average time clicks
+// TODO: only allocate and fill the memory once, not for each repitition
+//
 template <typename T>
 void matrixSumTimer(int size, int cols, T signal, int repitions, long *sum, long *avg){
-	Timer tm = Timer();
-	*sum = 0.0;
-	for(int i = 0; i < repitions; i++){
-		tm.begin();
-		matrixSum(size, cols, signal);
-		*sum += tm.delta();
-	}
-	*avg = *sum/repitions;
+  Timer tm = Timer();
+  *sum = 0.0;
+  for(int i = 0; i < repitions; i++){
+    tm.begin();
+    matrixSum(size, cols, signal);
+    *sum += tm.delta();
+  }
+  *avg = *sum/repitions;
 }
 
+// matrixMultiply multiplies two square matrices into a 3rd results matrix
+//  <size> is total #bytes, <block> is the blocking factor when looping, <signal> is the type
+//
+// Allocates a block of contiguous space, gives 1/3 for each matrix, calculates shared #rows/cols,
+// fills the source matrices with initial values, then loops through each r,c position in the
+// result filling it with sum of Source1[Row(r)] * Source2[Col(c)]
+// 
+// Rather then a simple loop of all rows by all cols, block factor allows calculating blocks of
+// results (e.g. [rows(0:31) x cols(0:31)], followed by [rows(32:63) x cols (0:31)] and so on
+// This blocking should have better cache usage as long as all the data for the set of rows and 
+// cols fit in the cache at once. By contrast, if we just looped over all cols, the first col
+// would be gone from the cache by the time we increment to the next row.
+//
 template <typename T>
-void matrixMultiply(int size, T signal){
-	int numElements = size/sizeof(T);
-	numElements /= 3; //we're splitting the buffer between 3 matrixes, so numElements will be the number of elements in each matrix
-	int n = (int)sqrt(numElements); //all the matricies will be square, so each matrix will be NxN
-	numElements = n*n; //Since we rounded down n to the nearest int, we update the value of numElements to make sure it's correct
-
-	T *buf = (T*)malloc(size);
-	T *buf2 = buf + numElements;
-	T *bufRes = buf2 + numElements;
-
-	// Set everything to an initial value
-	int r, c;
-	for (r = 0; r < n; r++) {
-	  T *rp = buf + r*n;
-	  T *rp2 = buf2 + r*n;
-	  for (c = 0; c < n; c++) {
-	    rp[c] = (T)(r+c);
-	    rp2[c] = (T)(n-r-c);
+int matrixMultiply(int size, int block, T signal){
+  int numElements = size/sizeof(T);
+  numElements /= 3; //we're splitting the buffer between 3 matrixes, so numElements will be the number of elements in each matrix
+  int n = (int)sqrt(numElements); //all the matricies will be square, so each matrix will be NxN
+  numElements = n*n; //Since we rounded down n to the nearest int, we update the value of numElements to make sure it's correct
+  
+  // Allocate the space and distribute across the three equal sized square matrices
+  T *buf1 = (T*)malloc(size);
+  T *buf2 = buf1 + numElements;
+  T *bufR = buf2 + numElements;
+  
+  // Set sources to an initial value
+  int x, r, c;
+  x = 0;
+  for (r = 0; r < n; r++) {
+    for (c = 0; c < n; c++) {
+      buf1[x] = (T)(r+c);
+      buf2[x] = (T)(n-r-c);
+      x += 1;
+    }
+  }
+  
+  // Make sure we have a reasonable blocking factor
+  if (block <= 0) block = 1;
+  
+  // Process the matrix in blocks to preserve cache 
+  // TODO: Explore whether it would be better to loop by c/r instead of r/c
+  for (r = 0; r < n; r += block) {
+    for (c = 0; c < n; c += block) {
+      // Calculate actual last r,c in the block
+      int rTgt = min(r + block, n);
+      int cTgt = min(c + block, n);
+      for (int rx = r; rx < rTgt; rx++) {
+	// Start of results for this row in the block
+	int x = rx * n + c;
+	for (int cx = c; cx < cTgt; cx++) {
+	  bufR[x] = 0;
+	  for(int j = 0; j < n; j++) {
+	    bufR[x] += buf1[rx*n + j] + buf2[j*n + cx];
 	  }
+	  x += 1;
 	}
+      }
+    }
+  }
+  
+  // Do something with the data so compiler doesn't optimize code away                                                             
+  int total = 0;
+  for (c = 0; c < n; c++) total += bufR[c];
 
-	//Multiply the matrixes at buf and buf2 and put the result in bufRes
-	for (int i = 0; i < numElements; i++){
-		//get the current row and column
-		r = i / n;
-		c = i % n;
-		bufRes[i] = 0;
-		for(int j = 0; j < n; j++){
-			bufRes[i] += buf[r*n + j] + buf2[j*n + c];
-		}
-	}
+  free(buf1);
 
-	// Sum the total result matrix to make sure compiler doesn't optimize code away
-	int total = 0;
-	for (c = 0; c < numElements; c++) total += bufRes[c];
-
-	free(buf);
+  return total;
 }
 
+// Time <repitition> runs of Multiply, returning total and average time clicks
+// TODO: only allocate and fill the memory once, not for each repitition
+//
 template <typename T>
-void matrixMultiplyTimer(int size, T signal, int repitions, long *sum, long *avg){
-	Timer tm = Timer();
-	*sum = 0.0;
-	for(int i = 0; i < repitions; i++){
-		tm.begin();
-		matrixMultiply(size, signal);
-		*sum += tm.delta();
-	}
-	*avg = *sum/repitions;
+void matrixMultiplyTimer(int size, int block, T signal, int repitions, long *sum, long *avg){
+  Timer tm = Timer();
+  *sum = 0.0;
+  for(int i = 0; i < repitions; i++){
+    tm.begin();
+    matrixMultiply(size, block, signal);
+    *sum += tm.delta();
+  }
+  *avg = *sum/repitions;
 }
 
 //for testing
@@ -125,7 +169,7 @@ void matrixMultiplyTimer(int size, T signal, int repitions, long *sum, long *avg
   std::cout << "Time Elapsed For Sum = " << tm.delta() << endl;
 
   tm.end();
-  matrixMultiply(size, 5);
+  matrixMultiply(size, 1, 5);
   std::cout << "Time Elapsed For Multiply = " << tm.delta() << endl;
 
   matrixSumTimer(size, cols, 5, 5, &sum, &avg);
